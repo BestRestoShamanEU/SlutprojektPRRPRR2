11999e089b9e61ac415cf08ece1cd31c1fbcee62
BestRestoShamanEU <42673669+BestRestoShamanEU@users.noreply.github.com>
2020-05-20 12:57:26 +0200

    1

--- tictactoe/Tictactoe.java
+++ tictactoe/Tictactoe.java
@@ -1,10 +1,19 @@
-public class Tictactoe {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
+public class Tictactoe extends Application {
+
+
+
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // TODO Auto-generated method stub
+        primaryStage.setScene(new Scene(createContent()));
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
-        
-
-
-
-    }
+        launch(args); 
+    }   
 }
