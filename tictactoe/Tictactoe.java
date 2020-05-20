abb7448086c8be5153802fe0d64441898c348b18
BestRestoShamanEU <42673669+BestRestoShamanEU@users.noreply.github.com>
2020-05-20 13:05:30 +0200

    Just nu så öppnar applikationen bara ett fönster.

--- tictactoe/Tictactoe.java
+++ tictactoe/Tictactoe.java
@@ -1,19 +1,29 @@
 import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Tictactoe extends Application {
 
+    private Parent createContent() {
+        final Pane root = new Pane();
+        root.setPrefSize(600, 600);
+
+        return root;
+        //Anger storlek på rotbehållaren
+    }
 
 
-    
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(final Stage primaryStage) throws Exception {
         // TODO Auto-generated method stub
         primaryStage.setScene(new Scene(createContent()));
         primaryStage.show();
+     //Detta är fönstret, scenen är innehållet, anropar createContent.
     }
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         launch(args); 
     }   
 }
